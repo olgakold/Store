94db633e555a41729bf16b6035bfda8e32eeff0a
olgakold <64192555+olgakold@users.noreply.github.com>
2020-07-15 02:15:23 +0300

    Add files via upload

---
+++ _tests_/LoginButton-test.js
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Login from '../components/LogIn';
+
+test('работа Login', ()=>{
+  let viewLogin=1
+  const component = renderer.create(
+        <Login viewLogin={viewLogin}/>
+  ); 
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  const buttonElem = component.root.find( el => el.props.value=='Запомнить');   
+  buttonElem.props.onClick();
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  buttonElem.props.onClick();
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+    
+});
+
+test('работа Login', ()=>{
+  let viewLogin=2
+  const component = renderer.create(
+        <Login viewLogin={viewLogin}/>
+  ); 
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  const buttonElem = component.root.find( el => el.props.value=='Войти');   
+  buttonElem.props.onClick();
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  buttonElem.props.onClick();
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+    
+});
